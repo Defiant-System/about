6949fb2b8e528dbed55528d177e6fb3e349f0ecc
Hakan Bilgin <hbi99@hotmail.com>
2020-11-08 11:17:31 +0100

    license view

--- src/js/main.js
+++ src/js/main.js
@@ -53,25 +53,7 @@ const about = {
 				let xApp = window.bluePrint.selectSingleNode(`sys://Application[.//meta/@name="id" and .//meta/@value="${Self.app}"]`);
 				let size = xApp.xml.replace(/ {4}/g, "").length;
 				Self.els.content.find(".size").html(defiant.formatBytes(size, 1));
-				/*
-				name = xApp.selectSingleNode(".//meta[@name='id']").getAttribute("value");
-				namespace = xApp.selectSingleNode(".//meta[@name='author']").getAttribute("namespace");
-				Self.els.icon.css({"background-image": `url(/app/${namespace}/icons/app-icon-${name}.png)`});
-
-				node = xApp.selectSingleNode(".//meta[@name='title']");
-				Self.els.name.html(node.getAttribute("value"));
-				Self.els.version.html(node.getAttribute("version"));
-
-				node = xApp.selectSingleNode(".//meta[@name='author']");
-				Self.els.author.html(node.getAttribute("value"));
-
-
-				let date = new Date(+xApp.getAttribute("mDate"));
-				Self.els.modified.html(date.toISOString().slice(0, 10));
 				
-				node = xApp.selectSingleNode(".//meta[@name='license']");
-				Self.els.license.html(node.getAttribute("value"));
-				*/
 				return true;
 			case "app-license":
 			case "app-issues":
